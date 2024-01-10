6c1560352fd76da2185e17eaf6e8108dffcb69d9
Jorge Muñoz García <91768274+JorgeBackendProjects@users.noreply.github.com>
2024-01-10 23:27:35 +0100

    Add files via upload

---
+++ Actividades/Tema 4/relacion_ejercicios_tema_4/src/main/java/com/mycompany/relacion_ejercicios_tema_4/Ejercicio_1.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.relacion_ejercicios_tema_4;
+
+/**
+ *
+ * @author Propietario
+ */
+public class Ejercicio_1 {
+    
+/**
+  
+ * 1. BorderLayout: Composición de borde, proporciona un esquema más complejo
+ * de colocación de los componentes en un panel. Este usa cinco zonas para
+ * colocar dichos componentes: Norte, sur, este, oeste y centro. 
+ * 
+ * Es el layout / composición por defecto en JFrame.
+
+ * 
+
+ * 2. BoxLayout: Es un layout simple que permite poner los componentes de 
+ * manera horizontal o vertical. 
+ * 
+ * Se asemeja a FlowLayout.
+
+ * 
+
+ * 3. CardLayout: Es un tipo de composición que se usa cuando se necesita una
+ * zona en la ventana para colocar componentes, en el rango de dicha zona.
+
+ * 
+
+ * 4. FlowLayout: Es el layout más simple, se usa por defecto en todos los
+ * paneles si no se determina otro concreto. Su distribución va de izquierda
+ * a derecha y de manera horizontal.
+
+ * 
+
+ * 5. GridBagLayout: Este layout permite colocar los componentes en forma de 
+ * matriz, haciendo que todos ocupen el mismo tamaño.
+
+ * 
+
+ * 6. GridLayout: Este layout se crea con un número de filas y columnas. Los
+ * componentes se sitúan en las celdas de la tabla.
+
+ * 
+
+ * 7. GroupLayout: Es un Layoutmanager que agrupa jerárquicamente los 
+ * componentes y los organiza en un contenedor. Esta agrupacion se realiza
+ * mediante instancias de la clase Group.
+
+ * 
+
+ * 8. SpringLayout: Estos layout definen relaciones direccionales o 
+ * restricciones entre los bordes de los componentes. 
+ 
+ *  
+*/
+    
+}
